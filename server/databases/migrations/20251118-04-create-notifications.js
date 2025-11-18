61c79076e6499e937af1f5599f82b24b22947ff8
jeongyeonjae0 <jeongyeonjae0@gamil.com>
2025-11-18 18:37:42 +0900

    251118 sequlize-cli.com

--- server/databases/migrations/20251118-04-create-notifications.js
+++ server/databases/migrations/20251118-04-create-notifications.js
@@ -1,13 +1,14 @@
 /**
- * @File databases/migrations/20251117-01-create-users.js
- * @description users migration file
- * 251117 v1.0.0 yeon init
+ * @File databases/migrations/20251118-04-create-notifications.js
+ * @description notifications migration file
+ * 251118 v1.0.0 yeon init
  */
 
 import { DataTypes } from 'sequelize';
 
+
 // 테이블명
-const tableName = 'users';
+const tableName = 'notifications';
 
 // 컬럼 정의
 const attributs = {
@@ -17,51 +18,32 @@ const attributs = {
     primaryKey: true,
     allowNull: false,
     autoIncrement: true,
-    comment: '유저 PK',
+    comment: '알림 PK',
   },
-  email: {
-    field: 'email',
-    type: DataTypes.STRING(100),
+  userId: {
+    field: 'user_id',
+    type: DataTypes.BIGINT.UNSIGNED,
     allowNull: false,
-    unique: true, 
-    comment: '이메일(로그인ID)'
+    comment: '유저 PK'
   },
-  password: {
-    field: 'password',
-    type: DataTypes.STRING(255),
+  title: {
+    field: 'title',
+    type: DataTypes.STRING(200),
     allowNull: false,
-    comment: '비밀번호',
+    comment: '제목'
   },
-  nick: {
-    field: 'nick',
-    type: DataTypes.STRING(15),
+  content: {
+    field: 'content',
+    type: DataTypes.STRING(1000),
     allowNull: false,
-    unique: true,
-    comment: '닉네임'
+    comment: '내용'
   },
-  provider: {
-    field: 'provider',
-    type: DataTypes.STRING(10),
+  isRead: {
+    field: 'is_read',
+    type: DataTypes.TINYINT(1),
     allowNull: false,
-    comment: '로그인 제공자(NONE, KAKAO, GOOGLE...)'
-  }, 
-  role: {
-    field: 'role',
-    type: DataTypes.STRING(10),
-    allowNull: false,
-    comment: '유저 권한(NOMAL, SUPER...)',
-  },
-  profile: {
-    field: 'profile',
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    comment: '유저 프로필',
-  },
-  refreshToken: {
-    field: 'refresh_token',
-    type: DataTypes.STRING(255),
-    allowNull: true,
-    comment: '리프레시 토큰', 
+    defaultValue: 0, 
+    comment: '읽음 여부'
   },
   createdAt: {
     field: 'created_at',
@@ -83,7 +65,7 @@ const attributs = {
 // 옵션
 const options = {
   charset: 'utf8mb4',       // 테이블 문자셋 설정 (이모지 지원)
-  collate: 'utf8mb4_bin',   // 정렬 방식 설정 (영어 대소문자 구분 정렬)
+  collate: 'utf8mb4_0900_ai_ci',   // 정렬 방식 설정 (기본 설정)
   engine: 'InnoDB'          // 사용 엔진 설정, 대량의 데이터 조회에 특화    
 };
 
---
+++ server/databases/seeders/dummy-users.seeder.js
@@ -0,0 +1,48 @@
+/**
+ * @file databases/seeders/dummy-users.seeder.js
+ * @description users table dummy data create
+ * 251118 v1.0.0 yeon init
+ */
+import bcrypt from 'bcrypt';
+
+// 테이블명 
+const tableName = 'users';
+
+
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+    // 레코드 정보 
+    const records = [
+      {
+        email: 'admin@admin.com',
+        password: await bcrypt.hash('qwe12312', 10),
+        nick: '미어캣관리자',
+        provider: 'NONE',
+        role: 'SUPER',
+        profile: '',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        email: 'admin2@admin.com',
+        password: await bcrypt.hash('qwe12312', 10),
+        nick: '미어캣관리자2',
+        provider: 'KAKAO',
+        role: 'NOMAL',
+        profile: '',
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ];
+
+    // 데이터 생성 : queryInterface.bulkInsert(tableName, records, options)
+    await queryInterface.bulkInsert(tableName, records, {}); 
+  },
+
+  async down (queryInterface, Sequelize) {
+    // 데이터 삭제 : queryInterface.bulkDelete(tableName, null, options)
+    await queryInterface.bulkDelete(tableName, null, {});
+  }
+};
