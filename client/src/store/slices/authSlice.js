4b9c0ec48ed8b3594204e29e44f5147b51f454b4
jeongyeonjae0 <jeongyeonjae0@gamil.com>
2025-12-02 18:43:34 +0900

    251202 인증 & 인가 처리 적용 및 로그인 처리

---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,41 @@
+import {createSlice} from '@reduxjs/toolkit';
+import { loginThunk, reissueThunk } from '../thunks/authThunk.js';
+
+const initialState = {
+  accessToken: null,
+  user: null,
+  isLoggedIn: false,
+}
+
+const slice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    clearAuth(state) {
+      state.accessToken = null;
+      state.user = null;
+      state.isLoggedIn = null;
+    }, 
+  },
+  extraReducers: (builder) => {
+    builder 
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        const { accessToken, user } = action.payload.data;
+        state.accessToken = accessToken;
+        state.user = user;
+        state.isLoggedIn = true;
+      })
+      .addCase(reissueThunk.fulfilled, (state, action) => {
+        const { accessToken, user } = action.payload.data;
+        state.accessToken = accessToken;
+        state.user = user;
+        state.isLoggedIn = true;
+      })
+  },
+});
+
+export const {
+  clearAuth,
+} = slice.actions; // redcuer에서 한 actions를 export, imort할 때 구조 분해 해서 사용
+
+export default slice.reducer; // slice 자체를 반환, store에서 받아서 사용
